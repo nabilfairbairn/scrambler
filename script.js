f61e34b9582c3687aecca1dc431d1675be1b2038
nabilfairbairn <118023687+nabilfairbairn@users.noreply.github.com>
2023-02-24 16:24:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+const wordrow_id_prefix = 'guess_number_'
+const words = ["TOUCH", "T_U__", "CO___"];
+const answers = [["TOUCH"], ["TOUGH"], ["COUGH"]]
+
 function limit(element) {
     var max_chars = 1;
     if(element.value.length > max_chars) {
         element.value = element.value.substr(0, max_chars);
     }
+}
+
+function process_input(element) {
+    // Remove extra letters
+    limit(element)
+    
+    // Remove letter styling if 'missing'
+    remove_letter_style(element)
+    
+    // Get parent word
+    var depth = element.depth
+    const word = document.getElementById(wordrow_id_prefix + depth.toString())
+    
+    // remove word style
+    remove_word_style(word)
+    
+    // remove word style from later words
+    depth++
+    while (depth < words.length) {
+        const [next_word, ng, na] = get_depth(depth) 
+        remove_word_style(next_word)
+        depth++
+    }
+    
 }
 
 function count_letters(str){
@@ -56,12 +84,176 @@ function one_letter_diff(word1, word2) {
     return (plus_one == 1 && minus_one == -1)
 }
 
+function get_depth(word_number) {
+  let guessid = wordrow_id_prefix + w.toString()
+  let guess_word = document.getElementById(guessid)
+  const answer_words = answers[w]
+  const guess_letters = guess_word.querySelectorAll('.letterBox')
+  var guess_received = ''
+
+  guess_letters.forEach((letter_box) => {
+    if (letter_box.classList.contains('letterInput')) {
+      guess_received += letter_box.value
+    } else {
+      guess_received += letter_box.innerText
+    }
+  })
+  return [guess_word, guess_received, answer_words]
+}
+
+function reaches_word(thisword, thisdepth, wuc, wuc_depth) {
+    let joined_words = []
+    answers[thisdepth].forEach((word) => {
+        if (one_letter_diff(thisword, word)) {
+            joined_words.push(word)
+        }
+    })
+    if (thisdepth == wuc_depth) {
+        return joined_words.includes(wuc)
+    } else {
+        let reaches = true
+        joined_words.forEach((word) => {
+            if (!reaches_word(word, thisdepth+1, wuc, wuc_depth)) {
+                reaches = false
+            }
+        })
+        return reaches
+    }
+} 
+
+function get_word(word_number) {
+    let [guess_word, guess_received, answer_words] = get_depth(word_number)
+    return guess_received
+}
+
+function word_exists(word, answer_list) {
+    return answer_list.includes(word)
+}
+
+function word_complete(word, answer_list) {
+    return (word.length == answer_list[0].length)
+}
+
+function word_depth(wordrow) {
+  let wordrow_id = wordrow.id
+  let depth_str = wordrow_id.slice(wordrow_id_prefix.length, wordrow_id.length)
+  let depth = parseInt(depth_str)
+  return depth
+}
+
+function word_attempted(wordrow) {
+  const guess_letters = guess_word.querySelectorAll('.letterInput')
+
+  guess_letters.forEach((letter_box) => {
+    let letter = letter_box.value
+    if (!(letter == null || letter == '')) {
+        return true
+    }
+  }
+  return false
+}
+                        
+
+function is_word_valid(guess_word, guess_received, answer_words, valid_depths) {
+    // empty
+    if (!word_attempted(guess_word)) {
+        return 0
+    }
+    // not full guess
+    if (!word_complete(guess_received, answer_words)) {
+        return -1
+    }
+    // not in depth answers
+    if (!word_exists(guess_received, answer_words)) {
+        return -2
+    }
+    
+    let depth = word_depth(guess_word)
+    
+    // (word complete, and in answers)
+    // first word
+    if (depth == 0) {
+        return 1
+    }
+    // Only answer
+    if (answer_words.length == 1) {
+        return 1
+    }
+    // 
+    // Prev word valid?
+    if (valid_depths[depth-1]) {
+        // obeys rule
+        if (one_letter_diff(guess_received, get_word(depth-1))) {
+            return 1
+        }
+    }
+    // (prev_word not valid)
+    // Does closest valid word join this word?
+      
+    let nearest_valid_depth = -1
+    let i = depth
+    while (nearest_valid_depth == -1 && i > 0) {
+        nearest_valid_depth = valid_depths[i] ? i : nearest_valid_depth;
+        i--;
+    }
+    let valid_word = get_word(nearest_valid_depth)
+    if (reaches_word(valid_word, nearest_valid_depth, guess_received, depth) {
+        return 1
+    }
+    return 0
+}
+
+function puzzle_done(valid_words) {
+    let done = true
+    valid_words.forEach((validity) => {
+        if (!validity) {
+            done = false
+        }
+    })
+    return done
+}
+    
+function style_guessword(wordrow, validity) {
+    // 0: unattempted, 1: correct, -1: incomplete, -2: wrong
+    switch(validity) {
+        case 1:
+            wordrow.classList.add('correct')
+            break;
+        case -2:
+            wordrow.classList.add('wrong')
+        case -1:
+            const guess_letters = wordrow.querySelectorAll('.letterInput')
+            guess_letters.forEach((letterbox) => {
+                if (letterbox.value == null || letterbox.value == '') {
+                    letterbox.classList.add('missing')
+                }
+            })
+            break;
+            
+    }
+}
+    
+function remove_word_style(wordrow) {
+    wordrow.classList.remove('correct', 'wrong')
+}
+
+function remove_letter_style(letterbox) {
+    letterbox.classList.remove('missing')
+}
+
+function remove_all_word_style() {
+    const wordrows = document.querySelectorAll('.wordRow')
+    wordrows.forEach((wordrow) => {
+        remove_word_style(wordrow)
+        const letters = wordrow.querySelectorAll('.letterInput')
+        letters.forEach(letter => remove_letter_style(letter))
+    })
+}
+
 window.onload = function() {
   var input_id_answer = {}
   var input_id = 0
   var input_id_prefix = "user_input_"
-  var words = ["TOUCH", "T_U__", "CO___"];
-  var answers = [["TOUCH"], ["TOUGH"], ["COUGH"]]
 
   var openModalButtons = document.getElementById('modalbutton')
   var closeModalButtons = document.getElementById('closemodalbutton')
@@ -92,53 +284,39 @@ window.onload = function() {
   closeModalButtons.onclick = closeModal 
 
   document.getElementById("answerBtn").onclick = function() {
+    remove_all_word_style()
     var printing = ''
     var actual_answer = ''
     var curr_input_id = 0
+    
+    var valid_depths = []
 
     var w;
+    var one_letter_diff_shown = false
+    var rule_break_notice;
+    
     for (w = 0; w < words.length; w++) {
-      let guessid = 'guess_number_' + w.toString()
-      let guess_word = document.getElementById(guessid)
-      const answer_words = answers[w]
-      const guess_letters = guess_word.querySelectorAll('.letterBox')
-      var guess_received = ''
+      const [guess_word, guess_received, answer_words] = get_depth(w)
+      let validity_status = is_word_valid(guess_word, guess_received, answer_words, valid_depths) //-2 is invalid, -1 is incomplete, 0 is unattempted, 1 is valid
+      valid_depths.push(validity_status == 1 ? true : false)
+      style_guessword(guess_word, validity_status)
       
-      guess_letters.forEach((letter_box) => {
-          if (letter_box.classList.contains('letterInput')) {
-            guess_received += letter_box.value
-          } else {
-            guess_received += letter_box.innerText
-          }
-      })
-      if (answer_words.includes(guess_received)) {
-        guess_word.classList.add('correct')
-      }
-
-      
-      var w_word = words[w]
-      var l;
-      for (l = 0; l < w_word.length; l++) {
-        var l_letter = w_word[l]
-        if (l_letter === '_') {
-          var input_key = input_id_prefix + curr_input_id.toString()
-          var input_value = document.getElementById(input_key).value
-          input_value = input_value ? input_value.toUpperCase() : ''
-          printing += input_value
-          actual_answer += answers[w][l]
-          curr_input_id++;
-        } else {
-          printing += l_letter
-          actual_answer += l_letter
+      // If !one-letter-diff-shown and w < words.length-1 and w and w+1 are complete, check one-letter-diff. If violated, one-letter-diff-shown and show rule
+      if (!one_letter_diff_shown && w < words.length -1) {
+        const [next_word, next_received, next_answers] = get_depth(w+1)
+        if (word_complete(guess_received, answer_words) && word_complete(next_received, next_answers)) {
+            if (!one_letter_diff(guess_received, next_received)) {
+                one_letter_diff_shown = true
+                rule_break_notice = `Careful! You changed more than one letter between ${guess_received} and ${next_received}.`
+            }
         }
       }
-      printing += ' -> '
-      actual_answer += ' -> '
+      
     }
-    var right_wrong = (printing === actual_answer) ? "That's right!" : "Try again.";
+    var right_wrong = (puzzle_done(valid_depths)) ? "That's right!" : "Try again.";
 
     var hiding = document.getElementById("button_response")
-    hiding.innerText = right_wrong;
+    hiding.innerText = rule_break_notice ? rule_break_notice : right_wrong;
     hiding.style.visibility = "visible";
   }
 
@@ -156,7 +334,7 @@ window.onload = function() {
   for (let i = 0; i < n_words; i++) {
     var row = document.createElement("div");
     row.className = "wordRow";
-    row.id = 'guess_number_' + i.toString()
+    row.id = wordrow_id_prefix + i.toString()
     
 
     rowHolder.appendChild(row)
@@ -171,10 +349,8 @@ window.onload = function() {
         input.classList.add("letterBox")
         input.onkeydown = function() {limit(input)};
         input.onkeyup = function() {limit(input)};
+        input.setAttribute('depth', i)
 
-        input.id = input_id_prefix + input_id.toString();
-        input_id_answer[input.id] = answers[i][j]
-        input_id++;
         row.appendChild(input)
       } else {
         var letter_holder = document.createElement("div")
